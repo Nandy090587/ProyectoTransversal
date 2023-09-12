d4c397c8fa1ce389ae39458ffd8566d226f0d649
Nandy090587 <141645920+Nandy090587@users.noreply.github.com>
2023-09-12 11:39:11 -0300

    b

---
+++ src/universidadejemplo/AccesoADatos/InscripcionData.java
@@ -0,0 +1,20 @@
+
+package universidadejemplo.AccesoADatos;
+
+import universidadejemplo.Entidades.Inscripcion;
+
+
+public class InscripcionData {
+    
+    private Conexion con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+
+    public InscripcionData() {
+    }
+    
+    public void guardarInscripcion(Inscripcion ins){
+        
+    }
+    
+}
