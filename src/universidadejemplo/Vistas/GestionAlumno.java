c046db427d485231dc600562b3f84a4dffb90e69
diegoginard <diegoginard@hotmail.com>
2023-09-23 15:25:36 -0300

    tryCatch

--- src/universidadejemplo/Vistas/GestionAlumno.java
+++ src/universidadejemplo/Vistas/GestionAlumno.java
@@ -8,14 +8,15 @@ import universidadejemplo.AccesoADatos.*;
 import universidadejemplo.Entidades.*;
 
 public class GestionAlumno extends javax.swing.JInternalFrame {
-
+    
+    AlumnoData ad = new AlumnoData();
+    
     public GestionAlumno() {
         
         initComponents();
+        
          
     }
-    
-    AlumnoData ad = new AlumnoData();
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -201,9 +202,9 @@ public class GestionAlumno extends javax.swing.JInternalFrame {
 
     private void jbNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbNuevoActionPerformed
         
-        jtDni.setText("");
-        jtNombre.setText("");
-        jtApellido.setText("");
+        jtDni.setText(null);
+        jtNombre.setText(null);
+        jtApellido.setText(null);
         jrEstado.setSelected(false);
         jdFecha.setDate(null);
         
@@ -226,7 +227,7 @@ public class GestionAlumno extends javax.swing.JInternalFrame {
             }
         } catch (NumberFormatException ex) {
 
-            JOptionPane.showMessageDialog(null, "Ingrese un numero de Dni valido");
+            JOptionPane.showMessageDialog(this, "Ingrese un numero de dni valido");
 
         }
     }//GEN-LAST:event_jbBuscarActionPerformed
@@ -240,14 +241,50 @@ public class GestionAlumno extends javax.swing.JInternalFrame {
     private void jbGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbGuardarActionPerformed
         
         Alumno guardarAlum = new Alumno();
+        try{
+            
+            guardarAlum.setDni( Integer.parseInt(jtDni.getText()));
+            
+        }catch(NumberFormatException ex){
         
-        guardarAlum.setDni( Integer.parseInt(jtDni.getText()));
-        guardarAlum.setApellido(jtApellido.getText());
+            JOptionPane.showMessageDialog(this, "Ingrese un numero de dni");
+        
+        }
+        try{
+            
+            guardarAlum.setApellido(jtApellido.getText());
+            
+        }catch(Exception e1){
+            
+            JOptionPane.showMessageDialog(this, "Ingrese el apellido");
+            
+        }
+        
         guardarAlum.setNombre(jtNombre.getText());
-        guardarAlum.setFechaNacimiento(jdFecha.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        
+        try{
+            
+            guardarAlum.setFechaNacimiento(jdFecha.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+            
+        }catch(NullPointerException e2){
+            
+            JOptionPane.showMessageDialog(this, "Ingrese la fecha de nacimiento");
+            
+        }
+        
         guardarAlum.setActivo(jrEstado.isSelected());
         
-        ad.guardarAlumno(guardarAlum);
+        try{
+            
+            ad.guardarAlumno(guardarAlum);
+            
+        }catch(Exception e4){
+            
+            JOptionPane.showMessageDialog(this, "No se ingresaron correctamente los datos");
+            
+        }
+   
+        
         
     }//GEN-LAST:event_jbGuardarActionPerformed
 
